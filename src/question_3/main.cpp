801a5bc3445ca2b408f75c47a73376d289007f17
Dalton Tafolla <98137739+DaltonTaff@users.noreply.github.com>
2023-10-14 22:14:10 +0000

    Question 3

--- src/question_3/main.cpp
+++ src/question_3/main.cpp
@@ -1,6 +1,26 @@
+#include "question3.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
-int main()
+int main() 
 {
-
-    return 0;
+char input = 'y';
+while(input == 'y'){
+double sales;
+cout << "Enter number of sales: ";
+cin >> sales;
+while(sales<0){
+cout << "Invalid input:\n";
+cout << "Please enter number greater than 0:";
+cin >> sales;
+}
+double value = get_sales_commission(sales);
+cout << "Sales of " << sales << " has a commission of " << value <<".\n";
+cout << "If you want to continue press charcater y: ";
+cin >> input;
+if (input!='y')
+break;
+}
+return 0;
 }
--- src/question_3/question3.cpp
+++ src/question_3/question3.cpp
@@ -4,3 +4,15 @@ bool test_config()
 {
     return true;
 }
+
+double get_sales_commission(double num)
+{
+if(num>=0 && num<=499)
+return (num*0.05);
+else if(num>=500 && num<=999)
+return (num * 0.06);
+else if(num>=1000 && num<=1499)
+return (num*0.07);
+else
+return (num*0.08);
+}
